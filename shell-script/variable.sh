26f3eada00648253ab1de219641e89736540a057
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-11 13:39:46 +0530

    file added

--- shell-script/variable.sh
+++ shell-script/variable.sh
@@ -1,9 +1,9 @@
 $!/bin/bash
 
-1NAME="RoboShop"
-2NAME="DevOps"
+NAME1="RoboShop"
+NAME2="DevOps"
 
-echo "Hello, I am $1NAME, your virtual assistant for $2NAME."
+echo "Hello, I am $NAME1, your virtual assistant for $NAME2."
 echo "How can I assist you today?"
 echo "Please type your question or request below:"
-echo "$2NAME is here to help you with all your DevOps needs."
+echo "$NAME2 is here to help you with all your DevOps needs."
