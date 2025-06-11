4b88aa3396e8cdddbee2128c4e96e439c4bf6268
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-11 19:09:15 +0530

    create and modidied a file

--- shell-script/09-installation.sh
+++ shell-script/09-installation.sh
@@ -2,27 +2,22 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR :: You should be root user to run this script"
+if [ $USERID -ne 0 ]; then
+    echo "You should be root user to run this script"
     exit 1
-else
-    echo "you are a root user you can run this script"
 fi
 
-dnf list instaled mysql
+dnf list installed mysql
 
-if [ $? -ne 0 ]
-then 
-    echo "mysql is not installed, installing now..."
+if [ $? -ne 0 ]; then
+    echo "MySQL is not installed... going to install it"
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "mysql installed successfully"
+    if [ $? -eq 0 ]; then
+        echo "Installing MySQL is ... SUCCESS"
     else
-        echo "Failed to install mysql"
+        echo "Installing MySQL is ... FAILURE"
         exit 1
     fi
 else
-    echo "mysql is already installed"
+    echo "MySQL is already installed...Nothing to do"
 fi
