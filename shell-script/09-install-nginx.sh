3b874ae4a2ddd983bd9fcb8a21db0283df1d2a2b
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-12 10:47:21 +0530

    modified

--- shell-script/09-install-nginx.sh
+++ shell-script/09-install-nginx.sh
@@ -11,8 +11,6 @@ else
 fi
 
 dnf list installed nginx
-
-
 if [ $? -ne 0 ]
 then
     echo "nginx is not installed... going to install it"
@@ -26,4 +24,20 @@ then
     fi
 else
     echo "nginx is already installed...Nothing to do"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "Installing mysql is ... SUCCESS"
+    else
+        echo "Installing mysql is ... FAILURE"
+        exit 1
+    fi
+else
+    echo "mysql is already installed...Nothing to do"
 fi
